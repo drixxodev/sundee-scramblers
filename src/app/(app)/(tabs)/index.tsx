6d4505bfe9572fda4d8297a2322c5b80d5b31e9f
Robert Chambliss <robert@drixxo.dev>
2024-02-16 00:38:37 -0500

    Auth and Folder Structure
    Added auth and organized folder structure.

--- src/app/(app)/(tabs)/index.tsx
+++ src/app/(app)/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import React from 'react';
 
 import EditScreenInfo from '@/src/components/EditScreenInfo';
 import { Text, View } from '@/src/components/Themed';
