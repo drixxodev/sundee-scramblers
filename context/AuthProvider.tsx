4577d40a7c226de616ebdcc2e0c0b5d22f4b3f05
Robert Chambliss <robert@drixxo.dev>
2024-02-16 11:57:14 -0500

    Auth
    Added local auth no database

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -1,52 +1,59 @@
+import React from "react";
 import { useSegments, useRouter } from "expo-router";
 import { createContext, useContext, useEffect, useState } from "react";
 
 type User = {
-    name: string;
-}
+  name: string;
+};
 
 type AuthType = {
-    user: User | null;
-    setUser: (user: User | null) => void;
-}
+  user: User | null;
+  setUser: (user: User | null) => void;
+};
 
 const AuthContext = createContext<AuthType>({
-    user: null,
-    setUser: () => {},
+  user: null,
+  setUser: () => {},
 });
 
 export const useAuth = () => useContext(AuthContext);
 
 function useProtectedRoute(user: any) {
-    const segments = useSegments();
-    const router = useRouter();
+  const segments = useSegments();
+  const router = useRouter();
 
-    useEffect(() => {
-        const inAuthGroup = segments[0] === "(auth)";
+  useEffect(() => {
+    const inAuthGroup = segments[0] === "(auth)";
 
-        if (
-            // If the user is not signed in and the initial segment is not anything in the auth group.
-            !user &&
-            !inAuthGroup
-        ) {
-            // Redirect to the sign-in page.
-            router.replace("/login");
-        } else if (user && inAuthGroup) {
-            // Redirect away from the sign-in page.
-            router.replace("/home");
-        }
-    }, [user, segments]);
+    if (
+      // If the user is not signed in and the initial segment is not anything in the auth group.
+      !user &&
+      !inAuthGroup
+    ) {
+      // Redirect to the sign-in page.
+      router.replace("/login");
+    } else if (user && inAuthGroup) {
+      // Redirect away from the sign-in page.
+      router.replace("/home");
+    }
+  }, [user, segments]);
 }
 
-export function AuthProvider({ children }: { children: JSX.Element }): JSX.Element {
-    const [user, setUser] = useState<User | null>(null);
+export function AuthProvider({
+  children,
+}: {
+  children: JSX.Element;
+}): JSX.Element {
+  const [user, setUser] = useState<User | null>(null);
 
-    useProtectedRoute(user);
+  useProtectedRoute(user);
 
-    const authContext: AuthType = {
-        user,
-        setUser,
-    };
+  const authContext: AuthType = {
+    user,
+    setUser,
+  };
 
-    return <AuthContext.Provider value={authContext}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={authContext}>{children}</AuthContext.Provider>
+  );
 }
